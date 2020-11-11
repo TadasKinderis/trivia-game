ead4594a92269ec75349b44dc3233750fa4b7626
Tadas Kinderis <tadas.kinderis@teliacompany.com>
2020-11-11 12:01:59 +0200

    Suppressed 'simple using statement' messages

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:TriviaGame.Services.ApiClient.GetCategoriesAsync~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{TriviaGame.Models.TriviaCategory}}")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:TriviaGame.Services.ApiClient.GetQuestionsAsync(TriviaGame.Models.QueryOptions)~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{TriviaGame.Models.Result}}")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:TriviaGame.Services.ApiClient.GetTokenAsync~System.Threading.Tasks.Task")]
